0a054839c3637be9d76e7f13349f62a39c2987ab
chenyuanxing <chenyuanxing_bupt@foxmail.com>
2019-01-19 14:57:57 +0800

    test

--- beatmanage/heartbeat.go
+++ beatmanage/heartbeat.go
@@ -27,6 +27,9 @@ func Heart(uuid,user string)  {
 			fmt.Println("Heart - Marshal Error: ", err.Error());
 			return
 		}
+		fmt.Println("heart message:")
+		fmt.Println(params)
+
 		params.Set("collectionStatuses",string(buf))
 		http.PostForm("http://10.108.210.194:8080/agents/aliveAgent",params)
 
