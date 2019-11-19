32a0748b0797724ab5c27e39afe3209baf25b795
chenyuanxing <chenyuanxing_bupt@foxmail.com>
2019-11-19 15:50:09 +0800

    修改后台连接端口

--- beatmanage/heartbeat.go
+++ beatmanage/heartbeat.go
@@ -8,30 +8,30 @@ import (
 	"time"
 )
 
-func Heart(uuid,user string)  {
+func Heart(uuid, user string) {
 	for range time.Tick(5 * time.Second) {
 		params := url.Values{}
-		params.Set("uuidKey",uuid)
+		params.Set("uuidKey", uuid)
 
-		params.Set("userId",user)
+		params.Set("userId", user)
 
 		// 发送心跳前将collection状态更新,只检查状态为on的
-		for i :=range CollectionStatusSlice{
-			if CollectionStatusSlice[i].Status=="on" {
+		for i := range CollectionStatusSlice {
+			if CollectionStatusSlice[i].Status == "on" {
 
 				updateCollectionStatus(&CollectionStatusSlice[i])
 			}
 		}
 
-		buf,err := json.Marshal(CollectionStatusSlice)
+		buf, err := json.Marshal(CollectionStatusSlice)
 		if err != nil {
-			fmt.Println("Heart - Marshal Error: ", err.Error());
+			fmt.Println("Heart - Marshal Error: ", err.Error())
 			return
 		}
-		params.Set("collectionStatuses",string(buf))
+		params.Set("collectionStatuses", string(buf))
 		//fmt.Println("heart message:")
 		//fmt.Println(params)
-		http.PostForm("http://10.108.210.194:8080/agents/aliveAgent",params)
+		http.PostForm("http://10.108.210.194:8999/agents/aliveAgent", params)
 
 	}
 }
--- beatmanage/register.go
+++ beatmanage/register.go
@@ -7,21 +7,21 @@ import (
 	"runtime"
 )
 
-func Regist(key,user,version,tag,port string)  {
+func Regist(key, user, version, tag, port string) {
 
 	params := url.Values{}
-	params.Set("uuidKey",key)
-	params.Set("agentVersion",version)
-	params.Set("tag",tag)
+	params.Set("uuidKey", key)
+	params.Set("agentVersion", version)
+	params.Set("tag", tag)
 
-	params.Set("system",runtime.GOOS)
-	params.Set("kernelVersion",runtime.GOARCH)
-	params.Set("port",port)
+	params.Set("system", runtime.GOOS)
+	params.Set("kernelVersion", runtime.GOARCH)
+	params.Set("port", port)
 
-	resp, _:= http.PostForm("http://10.108.210.194:8080/agents/registAgent",params)
+	resp, _ := http.PostForm("http://10.108.210.194:8999/agents/registAgent", params)
 	buf := make([]byte, 512)
 	resp.Body.Read(buf)
-	fmt.Println("regist data : \n" , params)
+	fmt.Println("regist data : \n", params)
 	fmt.Println("register result : \n", string(buf[:]))
 
 }
